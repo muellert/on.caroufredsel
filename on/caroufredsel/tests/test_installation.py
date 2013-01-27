f11ed0f1ae4637f6774e03762a7e30c3683b582b
Toni Mueller <dev@tonimueller.org>
2013-01-27 03:49:25 +0100

    actually add a testcase

---
+++ on/caroufredsel/tests/test_installation.py
@@ -0,0 +1,29 @@
+
+import unittest2 as unittest
+
+#from zope.component import queryUtility
+
+from Products.CMFCore.utils import getToolByName
+
+from plone.registry.interfaces import IRegistry
+from plone.app.testing import TEST_USER_ID
+from plone.app.testing import setRoles
+
+from on.caroufredsel.testing import ON_CAROUFREDSEL_INTEGRATION_TESTING
+
+
+class TestLibInRegistry(unittest.TestCase):
+    """Test the integration of the product."""
+
+    layer = ON_CAROUFREDSEL_INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+
+    def test_can_we_find_the_library(self):
+        """Get the registry and see whether we have this lib listed."""
+        tool = getToolByName(self.portal, "portal_javascripts")
+        self.assertNotEqual(tool, None)
+        installedScriptIds = tool.getResourceIds()
+        self.assertTrue("caroufredsel.js" in installedScriptIds, "caroufredsel.js")
