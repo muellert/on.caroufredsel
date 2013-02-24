c5529b446dd90b8f36b877ac2e85288cf85f4f27
Toni Mueller <dev@tonimueller.org>
2013-02-24 01:44:11 +0100

    remove unwanted code

--- on/caroufredsel/testing.py
+++ on/caroufredsel/testing.py
@@ -1,11 +1,8 @@
-
-import unittest
-
-#from zope.testing import doctestunit
-#from zope.component import testing
+# (c) 2012 oeko.net
+# c/o toni mueller <support@oeko.net>
+# license: GPLv3
 
 import unittest2 as unittest
-#from Products.CMFCore.utils import getToolByName
 
 from plone.app.testing import PloneSandboxLayer
 from plone.app.testing import quickInstallProduct
@@ -28,13 +25,11 @@ from plone.testing import z2
 from plone.registry.interfaces import IRegistry
 from Products.CMFCore.utils import getToolByName
 
-
-
 class OnCarouFredSelFixture(PloneSandboxLayer):
     defaultBases = (PLONE_FIXTURE,)
 
     def setUpZope(self, app, configurationContext):
-	import on.caroufredsel
+        import on.caroufredsel
         self.loadZCML(package=on.caroufredsel)
         self.loadZCML(package=Products.CMFPlone)
 
@@ -50,40 +45,13 @@ class OnCarouFredSelFixture(PloneSandboxLayer):
         """Uninstall the product and destroy the Zope site"""
         z2.uninstallProduct(app, 'on.caroufredsel')
 
-    def manager_browser(self):
-        """Browser of Manager - not accessible from test layer
-        """
-        return self._auth_browser(SITE_OWNER_NAME, SITE_OWNER_PASSWORD)
+#    def manager_browser(self):
+#        """Browser of Manager - not accessible from test layer
+#        """
+#        return self._auth_browser(SITE_OWNER_NAME, SITE_OWNER_PASSWORD)
 
 
 ON_CAROUFREDSEL_FIXTURE = OnCarouFredSelFixture()
 ON_CAROUFREDSEL_INTEGRATION_TESTING = IntegrationTesting(bases=(ON_CAROUFREDSEL_FIXTURE,), name="OnCarouFredSelFixture:Integration")
 ON_CAROUFREDSEL_FUNCTIONAL_TESTING = FunctionalTesting(bases=(ON_CAROUFREDSEL_FIXTURE,), name="OnCarouFredSelFixture:Functional")
 
-
-def test_suite():
-    return unittest.TestSuite([
-
-        # Unit tests
-        #doctestunit.DocFileSuite(
-        #    'README.txt', package='on.caroufredsel',
-        #    setUp=testing.setUp, tearDown=testing.tearDown),
-
-        #doctestunit.DocTestSuite(
-        #    module='on.caroufredsel.mymodule',
-        #    setUp=testing.setUp, tearDown=testing.tearDown),
-
-
-        # Integration tests that use PloneTestCase
-        #ztc.ZopeDocFileSuite(
-        #    'README.txt', package='on.caroufredsel',
-        #    test_class=TestCase),
-
-        #ztc.FunctionalDocFileSuite(
-        #    'browser.txt', package='on.caroufredsel',
-        #    test_class=TestCase),
-
-        ])
-
-if __name__ == '__main__':
-    unittest.main(defaultTest='test_suite')
